e8a638ced6ce94b748e2f50b8d5fd4462d1aa2da
clarkinada <clark.inada@hart.com>
2016-01-22 23:33:45 -0800

    removed comment

--- craydent-1.8.1.js
+++ craydent-1.8.1.js
@@ -6448,7 +6448,6 @@ if (__thisIsNewer) {
             });
 
             for (var i = 0, len = arr.length; i < len; i++) {
-                //var prop = _groupFieldHelper(arr[i], $c.getKeys(arr[i]));
                 arr[i] = finalize($c.merge(arr[i],result[arr[i][id]]));
             }
             return arr;
